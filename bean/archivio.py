2eda1e9b2172bc30ea796c888f48b121ea932f14
Mario Cetrangolo <55892147+CetrangoloMario@users.noreply.github.com>
2022-01-19 23:20:20 +0100

    Creazione Registrazione, Da fare urgente DB e model, DB SQL, funzione otp email

---
+++ bean/archivio.py
@@ -0,0 +1,5 @@
+class Archivio:#si aggingono mano mano i tag .... tipo di compressione criptazione scelta.
+    def __init__(self, utente:str =None, nome:str = None):
+        self.name_user=utente
+        self.nome=nome
+        
