34ee3fcb468c945fdc58dba2666a8664e1349602
Mario Cetrangolo <55892147+CetrangoloMario@users.noreply.github.com>
2022-02-06 12:55:04 +0100

    sql db

--- category_archive_db.sql
+++ category_archive_db.sql
@@ -1,138 +1,44 @@
--- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
---
--- Host: localhost    Database: category_archive_db
--- ------------------------------------------------------
--- Server version	8.0.16
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8 ;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+DROP TABLE IF EXISTS blob;
+CREATE TABLE blob (
+  name varchar(20) NOT NULL,
+  name_container varchar(45) NOT NULL,
+  crypto varchar(50) DEFAULT NULL,
+  compression varchar(50) DEFAULT NULL,
+  PRIMARY KEY (name),
+  UNIQUE KEY name_UNIQUE (name),
+  UNIQUE KEY name_container_UNIQUE (name_container),
+  CONSTRAINT name_container FOREIGN KEY (name_container) REFERENCES container (name) ON UPDATE CASCADE
+) 
 
---
--- Table structure for table `blob`
---
+DROP TABLE IF EXISTS container;
+CREATE TABLE container (
+  name varchar(30) NOT NULL,
+  nome_storage varchar(50) NOT NULL,
+  PRIMARY KEY (name),
+  UNIQUE KEY name_UNIQUE (name),
+  UNIQUE KEY nome_storage_UNIQUE (nome_storage),
+  KEY nome_storage_idx (nome_storage),
+  CONSTRAINT nome_storage FOREIGN KEY (nome_storage) REFERENCES storage (name) ON UPDATE CASCADE
+) 
 
-DROP TABLE IF EXISTS `blob`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `blob` (
-  `name` varchar(20) NOT NULL,
-  `name_container` varchar(45) NOT NULL,
-  `crypto` varchar(50) DEFAULT NULL,
-  `compression` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`name`),
-  UNIQUE KEY `name_UNIQUE` (`name`),
-  UNIQUE KEY `name_container_UNIQUE` (`name_container`),
-  CONSTRAINT `name_container` FOREIGN KEY (`name_container`) REFERENCES `container` (`name`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `blob`
---
+DROP TABLE IF EXISTS storage;
+CREATE TABLE storage (
+  name varchar(50) NOT NULL,
+  key varchar(200) NOT NULL,
+  iduser varchar(100) NOT NULL,
+  password varchar(30) NOT NULL,
+  PRIMARY KEY (name),
+  KEY iduser_idx (iduser),
+  CONSTRAINT iduser FOREIGN KEY (iduser) REFERENCES user (idUser) ON UPDATE CASCADE
+) 
 
-LOCK TABLES `blob` WRITE;
-/*!40000 ALTER TABLE `blob` DISABLE KEYS */;
-/*!40000 ALTER TABLE `blob` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `container`
---
-
-DROP TABLE IF EXISTS `container`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `container` (
-  `name` varchar(30) NOT NULL,
-  `nome_storage` varchar(50) NOT NULL,
-  PRIMARY KEY (`name`),
-  UNIQUE KEY `name_UNIQUE` (`name`),
-  UNIQUE KEY `nome_storage_UNIQUE` (`nome_storage`),
-  KEY `nome_storage_idx` (`nome_storage`),
-  CONSTRAINT `nome_storage` FOREIGN KEY (`nome_storage`) REFERENCES `storage` (`name`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `container`
---
-
-LOCK TABLES `container` WRITE;
-/*!40000 ALTER TABLE `container` DISABLE KEYS */;
-/*!40000 ALTER TABLE `container` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `storage`
---
-
-DROP TABLE IF EXISTS `storage`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `storage` (
-  `name` varchar(50) NOT NULL,
-  `key` varchar(200) NOT NULL,
-  `iduser` varchar(100) NOT NULL,
-  `password` varchar(30) NOT NULL,
-  PRIMARY KEY (`name`),
-  KEY `iduser_idx` (`iduser`),
-  CONSTRAINT `iduser` FOREIGN KEY (`iduser`) REFERENCES `user` (`idUser`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `storage`
---
-
-LOCK TABLES `storage` WRITE;
-/*!40000 ALTER TABLE `storage` DISABLE KEYS */;
-/*!40000 ALTER TABLE `storage` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `user` (
-  `idUser` varchar(100) NOT NULL,
-  `nomeRG` varchar(50) NOT NULL,
-  PRIMARY KEY (`idUser`),
-  UNIQUE KEY `idUser_UNIQUE` (`idUser`),
-  UNIQUE KEY `nomeRG_UNIQUE` (`nomeRG`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `user`
---
-
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping routines for database 'category_archive_db'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-02-06 12:12:55
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (
+  idUser varchar(100) NOT NULL,
+  nomeRG varchar(50) NOT NULL,
+  PRIMARY KEY (idUser),
+  UNIQUE KEY idUser_UNIQUE (idUser),
+  UNIQUE KEY nomeRG_UNIQUE (nomeRG)
+) 
